b7d50483d23b15c0b92bc56acf480d40156c0a9b
Sakai-Sangeon <85710206+Sakai-Sangeon@users.noreply.github.com>
2025-03-22 10:15:49 +0900

    Update component.js
    류 센세,  버튼 아이콘만 추가할려고 했는데 쳇쌤이 배경 이미지 추가까지 덤으로 해주신거 같습니다. 
    이 뒤에 아이콘 이미지 찾아서 추가하고 싶은데 이미지 파일은 어디서 어떻게 추가하는지 좀 알려주신다면 감사하겠습니다.

--- component.js
+++ component.js
@@ -1,28 +1,68 @@
-export class TextButton {
-  constructor(text, x, y, width, height, onClick, ctx) {
-    this.text = text;
+export class TextButton {                                 
+  constructor(text, x, y, width, height, onClick, ctx) {    
+    this.text = text;                                       
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
-    this.onClick = function (event) {
-      const mouseX = event.offsetX;
+    this.ctx = ctx;
+    this.image = null;
+    this.icon = null;
+    this,imageLoaded = false;
+    this.iconLoaded = false;
+
+    if(imageSrc) {
+      this.image = new Image();
+      this.image.src = imageSrc;
+      this.image.onload = () => {
+        this.imageLoaded = true;
+        this.draw();                                       
+      };
+    }
+    if(iconSrc) {
+      this.icon = new Image();
+      this.icon.src = iconSrc;
+      this.icon.onload = () => {
+        this.iconLoaded = true;
+        this.draw();
+      };
+    }
+
+
+    this.onClick = function (event) {                      
+      const mouseX = event.offsetX;                        
       const mouseY = event.offsetY;
 
       if (mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height) {
-        onClick();
+        onClick();                                         
       }
     };
-    this.ctx = ctx;
+    
   }
 
-  draw(isDisabled = false) {
-    this.ctx.fillStyle = isDisabled
-      ? "gray"
-      : "#4CAF50";
-    this.ctx.fillRect(this.x, this.y, this.width, this.height);
+  draw(isDisabled = false) {                              
+    if (this.imageLoaded) {
+      this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    } else {
+      this.ctx.fillStyle = isDisabled ? "gray" : "#4CAF50";
+      this.ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    
+    if (this.iconLoaded) {
+      let iconSize = 20;
+      let iconX = this.x + 10;                            
+      let iconY = this.y + (this.height - iconSize) 2;    
+      this.ctx.drawImage(this.icon, iconX, iconY, iconSize, iconSize);
+    }
+
     this.ctx.fillStyle = "white";
     this.ctx.font = "18px Arial";
-    this.ctx.fillText(this.text, this.x + this.width / 2, this.y + this.height / 2 + 6);
+    this.ctx.textAlign = "center";
+    this.ctx.textBaseline = "middle"
+    
+    let textX = this.icon ? this.x + this.width / 2 + 10 : this.x + this.width /2;
+    let textY = this.y + this.height / 2;
+    
+    this.ctx.fillText(this.text, thisX, thisY); 
   }
 }
