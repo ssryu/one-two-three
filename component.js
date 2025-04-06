31831c61e7823e2c3bc2c6abddc1ecf322d84fc4
ssryu <jjkztok3@gmail.com>
2025-04-06 18:21:58 +0900

    Revert onclick

--- component.js
+++ component.js
@@ -5,22 +5,31 @@ export class TextButton {
     this.y = y;
     this.width = width;
     this.height = height;
+    this.onClick = function (event) {
+      const mouseX = event.offsetX;
+      const mouseY = event.offsetY;
+
+      if (mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height) {
+        onClick();
+      }
+    };
     this.ctx = ctx;
-    this.onClick = onClick;
   }
 
   draw(isDisabled = false) {
-    this.ctx.fillStyle = isDisabled ? "gray" : "#4CAF50";
+    this.ctx.fillStyle = isDisabled
+      ? "gray"
+      : "#4CAF50";
     this.ctx.fillRect(this.x, this.y, this.width, this.height);
-    
+
     this.ctx.fillStyle = "white";
     this.ctx.font = "18px Arial";
     this.ctx.textAlign = "center";
     this.ctx.textBaseline = "middle";
-    
+
     let textX = this.x + this.width / 2;
     let textY = this.y + this.height / 2;
-    
+
     this.ctx.fillText(this.text, textX, textY);
   }
 }
@@ -40,7 +49,7 @@ export class IconButton extends TextButton {
 
   draw(isDisabled = false) {
     super.draw(isDisabled);
-    
+
     if (this.iconLoaded) {
       let iconSize = 20;
       let iconX = this.x + 10;
