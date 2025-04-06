bc503ee0109b5eb0bf68eb3033efe83032a56a21
ssryu <jjkztok3@gmail.com>
2025-04-06 19:55:42 +0900

    Update sound url

--- sounds/sounds.js
+++ sounds/sounds.js
@@ -4,16 +4,26 @@
  */
 
 /**
+ * github.io で sounds が404にならないようにする設定
+ */
+let prefix = "";
+let surfix = "";
+if (location.hostname === "ssryu.github.io") {
+  prefix = "https://github.com/ssryu/one-two-three/blob/main";
+  surfix = "?raw=true";
+}
+
+/**
  * BackGround Music
  *
  * 営利目的でないと利用できる:
  * https://www.nintendo.co.jp/networkservice_guideline/ja/index.html
  */
-export const bgm = new Audio("/sounds/GourmetRace.mp3");
+export const bgm = new Audio(`${prefix}/sounds/GourmetRace.mp3${surfix}`);
 
 /**
  * 無料効果音
  * https://taira-komori.jpn.org/arms01.html
  */
-export const bombSound = new Audio("/sounds/mini_bomb2.mp3");
-export const coinSound = new Audio("/sounds/coin05.mp3");
+export const bombSound = new Audio(`${prefix}/sounds/mini_bomb2.mp3${surfix}`);
+export const coinSound = new Audio(`${prefix}/sounds/coin05.mp3${surfix}`);
